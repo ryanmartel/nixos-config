4ecb31ea3efdac45cef4513128e29843c9bed736
Ryan Martel <martel.private@gmail.com>
2023-07-31 22:09:53 -0700

    finished initial restructure

--- flake.nix
+++ flake.nix
@@ -34,71 +34,41 @@
   # 
   # The `@` syntax here is used to alias the attribute set of the
   # inputs's parameter, making it convenient to use inside the function.
-  outputs = inputs@{ self, nixpkgs, home-manager,  ... }: {
-  # outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations = {
-      # By default, NixOS will try to refer the nixosConfiguration with
-      # its hostname, so the system named `nixos-test` will use this one.
-      # However, the configuration name can also be specified using:
-      #   sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>
-      #
-      # The `nixpkgs.lib.nixosSystem` function is used to build this
-      # configuration, the following attribute set is its parameter.
-      #
-      # Run the following command in the flake's directory to
-      # deploy this configuration on any NixOS system:
-      #   sudo nixos-rebuild switch --flake .#nixos-test
-      "RPM" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+  outputs = inputs@{ self, nixpkgs, home-manager,  ... }:  
+	let
+		system = "x86_64-linux"; #current system
+		pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+		lib = nixpkgs.lib;
 
-        # The Nix module system can modularize configuration,
-        # improving the maintainability of configuration.
-        #
-        # Each parameter in the `modules` is a Nix Module, and
-        # there is a partial introduction to it in the nixpkgs manual:
-        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
-        # It is said to be partial because the documentation is not
-        # complete, only some simple introductions.
-        # such is the current state of Nix documentation...
-        #
-        # A Nix Module can be an attribute set, or a function that
-        # returns an attribute set. By default, if a Nix Module is a
-        # function, this function can only have the following parameters:
-        #
-        #  lib:     the nixpkgs function library, which provides many
-        #             useful functions for operating Nix expressions:
-        #             https://nixos.org/manual/nixpkgs/stable/#id-1.4
-        #  config:  all config options of the current flake, every useful
-        #  options: all options defined in all NixOS Modules
-        #             in the current flake
-        #  pkgs:   a collection of all packages defined in nixpkgs,
-        #            plus a set of functions related to packaging.
-        #            you can assume its default value is
-        #            `nixpkgs.legacyPackages."${system}"` for now.
-        #            can be customed by `nixpkgs.pkgs` option
-        #  modulesPath: the default path of nixpkgs's modules folder,
-        #               used to import some extra modules from nixpkgs.
-        #               this parameter is rarely used,
-        #               you can ignore it for now.
-        #
-        # Only these parameters can be passed by default.
-        # If you need to pass other parameters,
-        # you must use `specialArgs` by uncomment the following line:
-        #
-        # specialArgs = {...}  # pass custom arguments into all sub module.
-        modules = [
-          # Import the configuration.nix here, so that the
-          # old configuration file can still take effect.
-          # Note: configuration.nix itself is also a Nix Module,
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ryan = import ./home;
-          }
-        ];
-      };
-    };
-  };
+		# Lets us reuse code to "create" a system
+		mkSystem = pkgs: system: hostname:
+			pkgs.lib.nixosSystem {
+				system = system;
+				modules = [
+					{ networking.hostName = hostname; }
+					# General config (users, network, sound, etc...)
+					./system/configuration.nix
+					# Hardware-config
+					(./. + "/hosts/${hostname}/hardware-configuration.nix")
+					home-manager.nixosModules.home-manager
+					{
+						home-manager = {
+							useGlobalPkgs = true;
+							useUserPackages = true;
+							extraSpecialArgs = { inherit inputs; };
+							users.ryan = (./. + "/home/${hostname}");
+						};
+					}
+				];
+				specialArgs = { inherit inputs; };
+			};
+
+		in {
+			nixosConfiguration = {
+				# Define Systems
+				#		              Architecture   Hostname
+				RPM = mkSystem inputs.nixpkgs "x86_64-linux" "RPM";
+			};
+		};
 }
+					
