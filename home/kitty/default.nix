1ade82cabcf749a21fc4601820f6870c80255e6d
Ryan Martel <martel.private@gmail.com>
2023-07-31 21:30:12 -0700

    first commit of nixos-config

---
+++ home/kitty/default.nix
@@ -0,0 +1,15 @@
+  {
+    programs.kitty = {
+      enable = true;
+      theme = "Tokyo Night";
+      font = {
+        name = "JetBrainsMono Nerd Font";
+        size = 11;
+      };
+      settings = {
+        background_opacity = "0.93";
+        scrollback_lines = 5000;
+        cursor-shape = "block";
+      };
+    };
+  }
