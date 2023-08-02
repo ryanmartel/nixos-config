eecddf382bc989b55abbf07dcaab3edce36ffae8
RMartel <martel.private@gmail.com>
2023-08-02 16:14:33 -0700

    color problems?

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -4,21 +4,32 @@ with lib;
 let cfg = config.modules.nvim;
 
 in {
-    options.modules.nvim = { enable = mkEnableOption "nvim"; };
-    config = mkIf cfg.enable {
-	programs.nixneovim = {
-		enable = true;
-		viAlias = true;
-		colorschemes.tokyonight = {
-			enable = true;
-			style = "night";
-			transparent = true;
-		};
-		options = {
-			number = true; #show line numbers
-			relativenumber = true;
-			shiftwidth = 2;
-		};
+  options.modules.nvim = { enable = mkEnableOption "nvim"; };
+  config = mkIf cfg.enable {
+    programs.nixneovim = {
+      enable = true;
+      viAlias = true;
+#		colorschemes.tokyonight = {
+#			enable = true;
+#			style = "day";
+#			transparent = true;
+#		};
+      options = {
+	number = true; #show line numbers
+	  relativenumber = true;
+	shiftwidth = 4;
+      };
+
+      # KEY MAPS
+      mappings = {
+      };
+
+      #PLUGINS
+      plugins = {
+	trouble {
+	  enable = true;
 	};
+      };
     };
+  };
 }
