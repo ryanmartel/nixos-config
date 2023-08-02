28f964d9d5006c4bd15ad54673bf36f78f0fbba1
RMartel <martel.private@gmail.com>
2023-08-02 16:31:05 -0700

    adding some plugs

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -4,32 +4,43 @@ with lib;
 let cfg = config.modules.nvim;
 
 in {
-  options.modules.nvim = { enable = mkEnableOption "nvim"; };
-  config = mkIf cfg.enable {
-    programs.nixneovim = {
-      enable = true;
-      viAlias = true;
+    options.modules.nvim = { enable = mkEnableOption "nvim"; };
+    config = mkIf cfg.enable {
+	programs.nixneovim = {
+	    enable = true;
+	    viAlias = true;
 #		colorschemes.tokyonight = {
 #			enable = true;
 #			style = "day";
 #			transparent = true;
 #		};
-      options = {
-	number = true; #show line numbers
-	  relativenumber = true;
-	shiftwidth = 4;
-      };
+	    options = {
+		number = true; #show line numbers
+		    relativenumber = true;
+		shiftwidth = 4;
+	    };
 
-      # KEY MAPS
-      mappings = {
-      };
+	    # KEY MAPS
+	    mappings = {
+	    };
 
-      #PLUGINS
-      plugins = {
-	trouble {
-	  enable = true;
+	    #PLUGINS
+	    plugins = {
+		trouble = {
+		    enable = true;
+		};
+		undotree = {
+		    enable = true;
+		};
+		treesitter = {
+		    enable = true;
+		    installAllGrammars = true;
+		};
+		treesitter-context = {
+		    enable = true;
+		    maxLines = 1;
+		};
+	    };
 	};
-      };
     };
-  };
 }
