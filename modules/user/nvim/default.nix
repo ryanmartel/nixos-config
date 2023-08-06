0da32b465aaad1d09de531ad86f4ac6f05508b72
ryanmartel <martel.private@gmail.com>
2023-08-06 09:36:12 -0700

    finished nvim config... for now

--- modules/user/nvim/default.nix
+++ modules/user/nvim/default.nix
@@ -7,14 +7,23 @@ in {
     options.modules.nvim = { enable = mkEnableOption "nvim"; };
     config = mkIf cfg.enable {
 	programs.nixvim = {
+
+        # General
 	    globals.mapleader = " ";
+        viAlias = true;
+        vimAlias = true;
 	    enable = true;
+
 	    # Make it pretty
 	    colorschemes.tokyonight = {
 	    	enable = true;
 		transparent = true;
 		style = "night";
 	    };
+
+        extraConfigLua = ''
+        vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+        '';
 
 	    # Options
 		options = {
@@ -30,7 +39,6 @@ in {
 			
 			swapfile = false;
 			backup = false;
-			undodir = "~/.vim/undodir";
 			undofile = true;
 
 			hlsearch = false;
@@ -39,7 +47,8 @@ in {
 			scrolloff = 8;
 			updatetime = 50;
 		};
-	    # HOTKEY MAPPING
+
+	    # General HOTKEY MAPPING
 	    maps = {
             # Harpoon
             normal."<C-e>" = { action = ''<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>''; };
@@ -82,85 +91,85 @@ in {
 	    };
 	    # Plugins
 	    plugins = {
-	    	fugitive.enable = true;
-		# auto-save.enable = true;
-		harpoon = {
-			enable = true;
-			keymaps = {
-			    addFile = "<leader>a";
-			   #cmdToggleQuickMenu = "<C-e>"; broken
-			    navFile = { "1" = "<C-h>"; "2" = "<C-j>"; "3" = "<C-k"; "4" = "<C-l>"; };
-			};
-		};
-		treesitter.enable = true;
-		treesitter-context = {
-			enable = true;
-			maxLines = 1;
-		};
-		telescope.enable = true;
-		trouble.enable = true;
-		undotree.enable = true;
-		presence-nvim = {
-		    enable = true;
-		};
-		nvim-autopairs.enable = true;
-		# LSP 
-		lsp = {
-		    enable  = true;
-		    servers = {
-			clangd.enable = true;
-			cmake.enable = true;
-			gopls.enable = true;
-			html.enable = true;
+#auto-save.enable = true;
+            fugitive.enable = true;
+            treesitter.enable = true;
+            telescope.enable = true;
+            trouble.enable = true;
+            undotree.enable = true;
+            nvim-autopairs.enable = true;
+            lualine.enable = true;
+            harpoon = {
+                enable = true;
+                keymaps = {
+                    addFile = "<leader>a";
+#cmdToggleQuickMenu = "<C-e>"; broken
+                    navFile = { "1" = "<C-h>"; "2" = "<C-j>"; "3" = "<C-k"; "4" = "<C-l>"; };
+                };
+            };
+            treesitter-context = {
+                enable = true;
+                maxLines = 1;
+            };
+            presence-nvim = {
+                enable = true;
+            };
+# LSP 
+            lsp = {
+                enable  = true;
+                servers = {
+                    clangd.enable = true;
+                    cmake.enable = true;
+                    gopls.enable = true;
+                    html.enable = true;
 # BROKEN?		java-language-server.enable = true;
-			jsonls.enable = true;
-			nixd.enable = true;
-			pylsp.enable = true;
-			rust-analyzer.enable = true;
-		    };
-		    onAttach = ''
-		    local opts = {buffer = bufnr, remap = false}
-		    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-		    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-		    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-		    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-		    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-		    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-		    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-		    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-		    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-		    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-		    '';
-		};
-		#rust-tools.enable = true;
-		cmp-nvim-lsp.enable = true;
-		cmp-buffer.enable = true;
-		cmp-path.enable = true;
-		cmp_luasnip.enable = true;
-		cmp-nvim-lua.enable = true;
-		luasnip.enable = true;
-		nvim-cmp = {
-		    enable = true;
-		    mapping = {
-			"<C-y>" = "cmp.mapping.confirm({ select = true })";
-			"<C-n>" = "cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select})";
-			"<C-p>" = "cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select})";
-			"<C-Space>" = "cmp.mapping.complete()";
-		    };
-		    snippet.expand = "luasnip";
-		    sources = [
-			{ name = "nvim_lsp"; }
-			{ name = "luasnip"; }
-			{ name = "path"; }
-			{ name = "buffer"; }
-		    ];
-		};
-		lualine.enable = true;
-		toggleterm = {
-		    enable = true;
-		    direction = "float";
-		    openMapping = "<A-1>";
-		};
+                    jsonls.enable = true;
+                    nixd.enable = true;
+                    pylsp.enable = true;
+                    rust-analyzer.enable = true;
+                };
+                onAttach = ''
+                    local opts = {buffer = bufnr, remap = false}
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+                    '';
+            };
+#rust-tools.enable = true;
+            cmp-nvim-lsp.enable = true;
+            cmp-buffer.enable = true;
+            cmp-path.enable = true;
+            cmp_luasnip.enable = true;
+            cmp-nvim-lua.enable = true;
+            luasnip.enable = true;
+            nvim-cmp = {
+                enable = true;
+                mapping = {
+                    "<C-y>" = "cmp.mapping.confirm({ select = true })";
+                    "<C-n>" = "cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select})";
+                    "<C-p>" = "cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select})";
+                    "<C-Space>" = "cmp.mapping.complete()";
+                };
+                snippet.expand = "luasnip";
+                sources = [
+                { name = "nvim_lsp"; }
+                { name = "luasnip"; }
+                { name = "path"; }
+                { name = "buffer"; }
+                ];
+            };
+            toggleterm = {
+                enable = true;
+                direction = "float";
+                openMapping = "<A-1>";
+            };
 	    };
 	};
     };
