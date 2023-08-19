2e35cc06dbc1f7bea5179f35700b991f447dbcb5
ryanmartel <martel.private@gmail.com>
2023-08-19 13:26:56 -0700

    formatting

--- modules/system/steam/default.nix
+++ modules/system/steam/default.nix
@@ -4,10 +4,10 @@ with lib;
 let cfg = config.modules.steam;
 
 in {
-    options.modules.steam = { enable = mkEnableOption "steam"; };
-    config = mkIf cfg.enable {
-	programs.steam = {
-	    enable = true;
+	options.modules.steam = { enable = mkEnableOption "steam"; };
+	config = mkIf cfg.enable {
+		programs.steam = {
+			enable = true;
+		};
 	};
-    };
 }
