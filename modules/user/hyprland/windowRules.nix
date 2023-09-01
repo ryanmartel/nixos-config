134fb08411d7b5ac8dd1e17c91d3f12107d80fb1
ryanmartel <martel.private@gmail.com>
2023-09-01 10:36:35 -0700

    got idleinhibit working

--- modules/user/hyprland/windowRules.nix
+++ modules/user/hyprland/windowRules.nix
@@ -4,5 +4,6 @@
 # Example windowrule v2
 # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
 # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
-windowrule = idleinhibit[fullscreen],*$
+windowrulev2 = idleinhibit fullscreen, class:^(Google-chrome)$
+windowrulev2 = idleinhibit focus, class:^(Google-chrome)$, title:^(.*YouTube.*)$
 ''
