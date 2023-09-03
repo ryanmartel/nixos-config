7eba2393c9611877cb0e69a92e32a851ba75d2ca
ryanmartel <martel.private@gmail.com>
2023-09-03 07:48:23 -0700

    finishing up initial package setup for Duke

---
+++ hosts/duke/users/ryan/user.nix
@@ -0,0 +1,18 @@
+{ config, lib, inputs, system, ... };
+
+{
+    imports = [
+        inputs.nixvim.homeManagerModules.nixvim
+        ./../../../../modules/user/default.nix
+        ./home.nix
+    ];
+    config = {
+        modules = {
+            git.enable = true;
+            bash.enable = true;
+            nvim.enable = true;
+            zsh.enable = true;
+            packages.enable = true;
+        };
+    };
+}
