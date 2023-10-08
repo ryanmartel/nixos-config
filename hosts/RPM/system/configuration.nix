86920b582433f0980609d885f42c65de93b62a00
ryanmartel <martel.private@gmail.com>
2023-10-08 14:22:51 -0700

    sync

--- hosts/RPM/system/configuration.nix
+++ hosts/RPM/system/configuration.nix
@@ -64,6 +64,9 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  services.avahi.openFirewall = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
