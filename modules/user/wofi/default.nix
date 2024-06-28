b596a10b40a67c6867b0dc71777c0678a41bae20
Ryan Martel <martel.private@gmail.com>
2024-06-28 07:34:52 -0700

    wofi styling

--- modules/user/wofi/default.nix
+++ modules/user/wofi/default.nix
@@ -8,7 +8,43 @@ in {
     config = mkIf cfg.enable {
         programs.wofi = {
             enable = true;
-            style = ''${./wofi.css}'';
+            style = ''
+                * {
+                    all: unset;
+                    font-family: "JetBrainsMono";
+                    font-size: 16px;
+                }
+
+                #window {
+                    background-color: #${config.colorScheme.palette.base02};
+                    border-radius: 12px;
+                }
+
+                #outer-box {
+                    background-color: #${config.colorScheme.palette.base02};
+                    border: 4px solid #${config.colorScheme.palette.base07};
+                    border-radius: 12px;
+                }
+
+                #input{
+                    margin: 1rem;
+                    padding: 0.5rem;
+                    border-radius: 10px;
+                    background-color: #${config.colorScheme.palette.base03};
+                }
+
+                #entry {
+                    margin: 0.25rem 0.75rem 0.25rem 0.75rem;
+                    padding: 0.25rem 0.75rem 0.25rem 0.75rem;
+                    color: #${config.colorScheme.palette.base07};
+                    border-radius: 8px;
+                }
+
+                #entry:selected {
+                    background-color: #${config.colorScheme.palette.base03};
+                    color: #${config.colorScheme.palette.base0F};
+                }
+            '';
         };
     };
 }
