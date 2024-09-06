0d7cb8441fbbdfc021c895bf493601cf70de4abf
ryanmartel <martel.private@gmail.com>
2024-09-05 17:51:15 -0700

    updating desktop to latest flake

--- hosts/RPM/users/ryan/user.nix
+++ hosts/RPM/users/ryan/user.nix
@@ -4,9 +4,13 @@
 	imports = [
         inputs.nix-colors.homeManagerModules.default
 		./../../../../modules/user/default.nix 
+        ./../../../../modules/user/background/default.nix
 		./home.nix
 	];
 	config = {
+        background = {
+            bgImage = ''${./../../../../static/eagle.png}'';
+        };
         colorScheme = {
             slug = "rpm";
             name = "rpm";
@@ -28,28 +32,15 @@
                 base0D = "#82AAFF";
                 base0E = "#C792EA";
                 base0F = "#FF5370";
-                # base00 = "#1d2021";
-                # base01 = "#3c3836";
-                # base02 = "#504945";
-                # base03 = "#665c54";
-                # base04 = "#bdae93";
-                # base05 = "#d5c4a1";
-                # base06 = "#ebdbb2";
-                # base07 = "#fbf1c7";
-                # base08 = "#fb4934";
-                # base09 = "#fe8019";
-                # base0A = "#fabd2f";
-                # base0B = "#b8bb26";
-                # base0C = "#8ec07c";
-                # base0D = "#83a598";
-                # base0E = "#d3869b";
-                # base0F = "#d65d0e";
             };
         };
 
 		modules = {
 			# GUI
             discord.enable = true;
+            qutebrowser.enable = true;
+            zathura.enable = true;
+            wofi.enable = true;
 
 			# CLI
 			kitty.enable = true;	
@@ -58,6 +49,7 @@
 			nvim.enable = true;
 			zsh.enable = true;
             direnv.enable = true;
+            ranger.enable = true;
 
 			# SYSTEM
 			packages.enable = true;
