bf9c31991b79fa80afbe62f8e0c372a69fc4606e
Ryan Martel <martel.private@gmail.com>
2024-09-18 08:01:31 -0700

    kanshi and docking on laptop

--- modules/user/kanshi/default.nix
+++ modules/user/kanshi/default.nix
@@ -16,6 +16,10 @@ in {
                 }
 
                 ];
+                profile.exec = [
+                    ''hyprctl keyword monitor eDP-1,1920x1200,0x0,1''
+                    ''hyprctl keyword unbind SUPER, P''
+                ];
             }
             { profile.name = "docked";
                 profile.outputs = [
@@ -27,6 +31,29 @@ in {
                     criteria = "eDP-1";
                     status = "disable";
                 }
+                ];
+                profile.exec = [
+                    ''hyprctl keyword monitor DP-1,3440x1440,0x0,1''
+                    ''hyprctl keyword unbind SUPER, P''
+                    ''hyprctl keyword bind SUPER, P, exec, kanshictl switch both''
+                ];
+            }
+            { profile.name = "both";
+                profile.outputs = [
+                {
+                    criteria = "DP-1";
+                    status = "enable";
+                }
+                {
+                    criteria = "eDP-1";
+                    status = "enable";
+                }
+                ];
+                profile.exec = [
+                    ''hyprctl keyword monitor DP-1,3440x1440,0x0,1''
+                    ''hyprctl keyword monitor eDP-1,1920x1200,3440x0,1''
+                    ''hyprctl keyword unbind SUPER, P''
+                    ''hyprctl keyword bind SUPER, P, exec, kanshictl switch docked''
                 ];
             }
             ];
